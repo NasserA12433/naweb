1ca191c3935770882c4f8e501222c48812adae23
NasserA12433 <nasseralnabulsi02@gmail.com>
2025-03-26 18:14:06 -0700

    Removed pages and packages. Will work on better home page next

--- src/home.tsx
+++ src/home.tsx
@@ -1,13 +1,15 @@
 import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./cssFiles/home.css";
-import CurrentPlans from "./currentplans.tsx";
 import CurrentProjects from "./currentprojects.tsx";
 import Experience from "./experience.tsx";
+
 
 function Home() {
   return (
     <div className="home-container">
-      <h1>Welcome to My Website</h1>
+      <h1>Im a </h1>
+      <span>
+      </span>
     </div>
   );
 }
@@ -20,14 +22,12 @@ function App() {
         <Link to="/" className="nav-button">Home</Link>
         <Link to="/experiences" className="nav-button">Experiences</Link>
         <Link to="/projects" className="nav-button">Projects</Link>
-        <Link to="/current-plans" className="nav-button">What I'm Working On</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/experiences" element={<Experience />} />
         <Route path="/projects" element={<CurrentProjects />} />
-        <Route path="/current-plans" element={<CurrentPlans />} />
       </Routes>
     </Router>
   );
