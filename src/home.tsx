f7bab1998fdb8842e399179fac78eb3469e9023f
NasserA12433 <nasseralnabulsi02@gmail.com>
2025-03-29 11:47:35 -0700

    Asset adding, Fixed front page. Will add resume button and other links.

--- src/home.tsx
+++ src/home.tsx
@@ -1,15 +1,27 @@
 import "./cssFiles/home.css";
 
+import { faLocationPin } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Typewriter from "./typewriter.tsx";
 
 function Home() {
   return (
+
     <div className="home-container">
-      <h1>Im a
-      <Typewriter text="Software Devloper" speed={100}/>
+      <img src="src\assets\Profile.png"/>
+      <h1>
+        Nasser Alnabulsi
       </h1>
+      <p>
+        <Typewriter text="Software Devloper " speed={100}/>
+        <FontAwesomeIcon icon={faLocationPin} size="2x" color="red"/>
+        {" "}La Verne California
+        <br/>
+        Learning and growing one line of code at a time
+        </p>
     </div>
   );
 }
+
 export default Home;
 
