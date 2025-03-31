074e0920da9c618cd02e90192e24c2a07dd6acd8
NasserA12433 <nasseralnabulsi02@gmail.com>
2025-03-31 12:36:46 -0700

    Slowly modifying the light dark theme options. Will go through and replace all the options later date

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './cssFiles/index.css';
 import Navbar from './navbar.tsx';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Navbar />
