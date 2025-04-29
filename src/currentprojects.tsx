01f940552a02320aaef85034eaf652855a964bd0
NasserA12433 <nasseralnabulsi02@gmail.com>
2025-04-28 21:27:36 -0700

    Added a holy cube. I will fix centering and then work on creating a stack of cubes

--- src/currentprojects.tsx
+++ src/currentprojects.tsx
@@ -1,5 +1,19 @@
-const CurrentProjects = () => {
-    return <h1>This is a blank page</h1>;
-};
+import './cssFiles/Cube.css';
 
-export default CurrentProjects;
+
+function Cube() {
+    return (
+        <div className="cube-container">
+        <div className="cube">
+        <div className="face front">React</div>
+        <div className="face back">2</div>
+        <div className="face right">3</div>
+        <div className="face left">4</div>
+        <div className="face top">5</div>
+        <div className="face bottom">6</div>
+        </div>
+        </div>
+    );
+}
+
+export default Cube;
