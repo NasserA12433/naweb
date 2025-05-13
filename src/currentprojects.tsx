ef9d857f2df8988a82b0fa4bf0c7a140838efdf6
NasserA12433 <nasseralnabulsi02@gmail.com>
2025-05-13 14:39:44 -0700

    Removed prop features

--- src/currentprojects.tsx
+++ src/currentprojects.tsx
@@ -4,14 +4,7 @@ import './cssFiles/cube.css';
 import Cube from './cube.tsx';
 
 
-interface ProjectPanelProps {
-	title:string;
-	cube:string;
-	description:string[];
-	images?:string[];
-	cubeElement?: React.ReactNode;
 
-}
 
 
 const CurrentProjects = () =>{
