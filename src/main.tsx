99e6a36eb337ce4e08cf5af57a1eefcee779fe9a
NasserA12433 <nasseralnabulsi02@gmail.com>
2025-05-26 21:11:18 -0700

    Removed button for reset. Added website icon

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import Navbar from './navbar.tsx';
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<ThemeProvider>
+			<link rel="icon" type="image/jpg" href="src\assets\favicon_c2.jpg"/>
 			<Navbar />
 		</ThemeProvider>
 	</StrictMode>,
